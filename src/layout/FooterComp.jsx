29db40b4a2467aa011de8f2f41477d6320dc0a98
Hari-repo <hariharanuk810@gmail.com>
2025-11-24 20:05:31 +0530

    Footer comp added

---
+++ src/layout/FooterComp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FooterComp = () => {
+    return(
+    <div className="footer-comp">   
+        <h2>Footer Component</h2>   </div>
+    )
+}   
+
+export default FooterComp;
