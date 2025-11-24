d8831528218595f570e959b9ea28b828d88d0fb4
Hari-repo <hariharanuk810@gmail.com>
2025-11-24 20:08:16 +0530

    Not found page custom page created and added

---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound404 = () => { 
+    return(
+    <div className="not-found-404">   
+        <h2>404 - Page Not Found</h2> </div>)
+
+}
+
+export default NotFound404;
